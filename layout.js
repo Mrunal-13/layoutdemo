d5f2f89a212a5100f7f0644834d4e5591efb6ce4
Mrunal-13 <mrunalnimbalkar13032001@gmail.com>
2022-05-24 13:25:45 +0530

    error removed

--- layout.js
+++ layout.js
@@ -16,27 +16,6 @@ let messagesDropdown = document.getElementById("messagesDropdown");
 let messageAlertDetails = document.getElementById("messageAlertDetails");
 messageAlertDetails.style.display = "none";
 
-// alert box for dropdown
-let alertsDropdown=document.getElementById("alertsDropdown");
-let alertDetails=document.getElementById("alertDetails");
-alertDetails.style.display="none";
-alertsDropdown.addEventListener("click",()=>{
-  if (alertDetails.style.display=="none") {
-    alertDetails.style.display="";
-    messageAlertDetails.style.display="none";
-
-  }
-  else{
-    alertDetails.style.display="none";
-  }
-})
-
-// dropdown for message box
-let messagesDropdown=document.getElementById("messagesDropdown");
-let messageAlertDetails=document.getElementById("messageAlertDetails");
-messageAlertDetails.style.display="none";
-
-
 messagesDropdown.addEventListener("click", () => {
     if (messageAlertDetails.style.display == "none") {
         messageAlertDetails.style.display = "";
@@ -111,37 +90,9 @@ dropbtnMyfunction.addEventListener("click", () => {
 })
 
 
-function showpateintTable() {
-    if (document.getElementById("pateintdropdown").style.display === 'none') {
-        document.getElementById('pateintdropdown').style.display = 'block';
-    }
-    // document.getElementsById('btnforpateints').style.background = 'yellow';
-    if (document.getElementById("navbar-fourth").style.width === "96%" || document.getElementById("navbar-fourth").style.width === "") {
-        document.getElementById("navbar-fourth").style.width = "78%";
-    } else if (document.getElementById("navbar-fourth").style.width === "83.5%") {
-        document.getElementById("navbar-fourth").style.width = "65%";
-    } else if (document.getElementById("pateintdropdown").style.display === "block") {
-        document.getElementById("pateintdropdown").style.display = "none";
-        if (document.getElementById("navbar-fourth").style.width === "65%") {
-            document.getElementById("navbar-fourth").style.width = "83.5%";
-        } else if (document.getElementById("navbar-fourth").style.width === "78%") {
-            document.getElementById("navbar-fourth").style.width = "96%";
-        }
-// dropdown for admin setting box
-
-let adminSettingBox=document.getElementById("adminSettingBox");
-adminSettingBox.style.display="none";
-let dropbtnMyfunction=document.getElementById("dropbtnMyfunction");
-dropbtnMyfunction.addEventListener("click",()=>{
-  if (adminSettingBox.style.display==="none") {
-      adminSettingBox.style.display="";
-  } else {
-    adminSettingBox.style.display="none";
-  }
-})
-
 // button for pateimt table
-function showpateintTable(){
+function showpateintTable()
+{
   if ( document.getElementById("pateintdropdown").style.display === 'none'){
     document.getElementById('pateintdropdown').style.display = 'block';
   }
@@ -171,6 +122,7 @@ function showpateintTable(){
     } else if (document.getElementById("Appointment").style.marginLeft === "10px") {
         document.getElementById("Appointment").style.marginLeft = "300px"
     }
+}
 
 }
 
@@ -184,19 +136,6 @@ function makelist() {
     });
 }
 
-
-document.getElementById('inputforhistory').addEventListener('keypress', function(e) {
-    if (e.key == 'Enter') {
-
-        var text = document.getElementById('inputforhistory').value;
-        document.getElementById('inputforhistory').value = null;
-        document.getElementById('inputforhistory').style.display = 'none';
-        var li = document.createElement('li');
-        li.innerHTML = text + "  " + "<img class='minus' src='https://img.icons8.com/small/16/000000/minus.png'/>";
-        var empyttext = document.getElementById('inputforhistory').value;
-        if (empyttext != null) {
-            document.getElementById('MedicalHistory').appendChild(li);
-        }
 // making list for mdeical history
 document.getElementById('inputforhistory').addEventListener('keypress', function(e){
   if(e.key == 'Enter'){
@@ -211,15 +150,8 @@ document.getElementById('inputforhistory').addEventListener('keypress', function
       document.getElementById('MedicalHistory').appendChild(li);
 
     }
+  }
 });
-
-
-function makelist1() {
-    document.getElementById('inputfordiseases').style.display = 'block';
-    $('.minus').click(function(e) {
-        e.target.parentElement.remove();
-    });
-}
 
 document.getElementById('inputfordiseases').addEventListener('keypress', function(e) {
 
@@ -233,6 +165,8 @@ document.getElementById('inputfordiseases').addEventListener('keypress', functio
         if (empyttext != null) {
             document.getElementById('otherdisease').appendChild(li);
         }
+    }
+});
 // input box for other specified diseases
 function makelist1(){
   document.getElementById('inputfordiseases').style.display = 'block';
@@ -255,14 +189,9 @@ document.getElementById('inputfordiseases').addEventListener('keypress', functio
       document.getElementById('otherdisease').appendChild(li);
 
     }
+  }
 });
 
-
-function makelist2() {
-    document.getElementById('inputfordrug').style.display = 'block';
-    $('.minus').click(function(e) {
-        e.target.parentElement.remove();
-    });
   
 // input for drug section
 function makelist2(){
@@ -288,12 +217,6 @@ document.getElementById('inputfordrug').addEventListener('keypress', function(e)
     }
 });
 
-
-function makelist3() {
-    document.getElementById('inputforfood').style.display = 'block';
-    $('.minus').click(function(e) {
-        e.target.parentElement.remove();
-    });
 // input for food
 function makelist3(){
   document.getElementById('inputforfood').style.display = 'block';
@@ -316,13 +239,6 @@ document.getElementById('inputforfood').addEventListener('keypress', function(e)
         }
     }
 });
-
-function makelist4() {
-    document.getElementById('inputforinsect').style.display = 'block';
-    $('.minus').click(function(e) {
-        e.target.parentElement.remove();
-    });
-
 // add button and input for insect
 function makelist4(){
   document.getElementById('inputforinsect').style.display = 'block';
@@ -345,14 +261,6 @@ document.getElementById('inputforinsect').addEventListener('keypress', function(
         }
     }
 });
-
-
-function makelist5() {
-    document.getElementById('inputforlatex').style.display = 'block';
-    $('.minus').click(function(e) {
-        e.target.parentElement.remove();
-    });
-
 // input for latex
 
 function makelist5(){
@@ -378,14 +286,6 @@ document.getElementById('inputforlatex').addEventListener('keypress', function(e
     }
 });
 
-
-function makelist6() {
-    document.getElementById('inputformold').style.display = 'block';
-    $('.minus').click(function(e) {
-        e.target.parentElement.remove();
-    });
-
-
 // this are input mold
 function makelist6(){
   document.getElementById('inputformold').style.display = 'block';
@@ -409,13 +309,6 @@ document.getElementById('inputformold').addEventListener('keypress', function(e)
         }
     }
 });
-
-
-function makelist7() {
-    document.getElementById('inputforpet').style.display = 'block';
-    $('.minus').click(function(e) {
-        e.target.parentElement.remove();
-    });
 
 // input section for pet
 function makelist7(){
@@ -447,14 +340,6 @@ function makelist8() {
     $('.minus').click(function(e) {
         e.target.parentElement.remove();
     });
-
-// input section for pollen
-function makelist8(){
-  document.getElementById('inputforpollen').style.display = 'block';
-  $('.minus').click(function(e){
-    e.target.parentElement.remove();
-  });
-
 }
 
 document.getElementById('inputforpollen').addEventListener('keypress', function(e) {
@@ -517,63 +402,6 @@ function openAppointment() {
         f[i].style.display = "none";
     }
 }
-
-
-
-function openPayments() {
-    document.getElementById("VeiwsAndPaymentsbutton").style.backgroundColor = "rgb(237, 97, 162)";
-    document.getElementById("VeiwsAndPaymentsbutton").style.color = "black";
-    document.getElementById("VisitReportButton").style.backgroundColor = "white";
-    document.getElementById("VisitReportButton").style.color = "grey";
-    document.getElementById("VisitReportButton").style.borderWidth = "1px";
-    document.getElementById("VisitReportButton").style.borderStyle = "solid";
-    document.getElementById("VisitReportButton").style.borderColor = "grey";
-    document.getElementById("ReadyCaseFormatButton").style.backgroundColor = "white";
-    document.getElementById("ReadyCaseFormatButton").style.color = "grey";
-    if (document.getElementById("AppointmentDetails").style.display = "block") {
-        document.getElementById("AppointmentDetails").style.display = "none";
-        document.getElementById("VeiwsAndPaymentsContent").style.display = "block"
-        document.getElementById("ReadyCaseFormatContent").style.display = "none";
-        document.getElementById("iframes").style.display = "none";
-    } else if (document.getElementById("AppointmentDetails").style.display = "none") {
-        document.getElementById("VeiwsAndPaymentsContent").style.display = "block";
-        document.getElementById("ReadyCaseFormatContent").style.display = "none";
-        document.getElementById("iframes").style.display = "none";
-    }
-}
-
-function Billingp() {
-    document.getElementById("Billingt").style.display = "block";
-    document.getElementById("Billingp").style.backgroundColor = "yellow";
-    document.getElementById("paymenthistoryp").style.backgroundColor = "white";
-    document.getElementById("paymenthistoryt").style.display = "none";
-}
-
-
-function paymenthistory() {
-    document.getElementById("Billingp").style.backgroundColor = "white";
-    document.getElementById("paymenthistoryp").style.backgroundColor = "yellow";
-    document.getElementById("Billingt").style.display = "none";
-    document.getElementById("paymenthistoryt").style.display = "block";
-}
-
-
-function veiwHistory() {
-    document.getElementById("veiwHistoryTable").style.display = "block";
-}
-
-
-function OpenReadyCase() {
-    document.getElementById("ReadyCaseFormatButton").style.backgroundColor = "rgb(237, 97, 162)";
-    document.getElementById("ReadyCaseFormatButton").style.color = "black";
-    document.getElementById("VeiwsAndPaymentsbutton").style.backgroundColor = "white";
-    document.getElementById("VeiwsAndPaymentsbutton").style.color = "grey";
-    document.getElementById("VisitReportButton").style.backgroundColor = "white";
-    document.getElementById("VisitReportButton").style.color = "grey";
-    document.getElementById("VisitReportButton").style.borderWidth = "1px";
-    document.getElementById("VisitReportButton").style.borderStyle = "solid";
-    document.getElementById("VisitReportButton").style.borderColor = "grey";
-
 // function to click on veiws and payments
 function openPayments(){
   document.getElementById("VeiwsAndPaymentsbutton").style.backgroundColor = "rgb(237, 97, 162)";
@@ -655,40 +483,13 @@ function VisitReport(){
   if(document.getElementById("ReadyCaseFormatContent").style.display = "block"){
     document.getElementById("ReadyCaseFormatContent").style.display = "none";
   }
-  if(document.getElementById("VeiwsAndPaymentsContent").style.display = "block"){
-
-    document.getElementById("VeiwsAndPaymentsContent").style.display = "none";
-    document.getElementById("iframes").style.display = "none";
-    document.getElementById("ReadyCaseFormatContent").style.display = "block";
-    if (document.getElementById("AppointmentDetails").style.display = "block") {
-        document.getElementById("AppointmentDetails").style.display = "none";
+  if(document.getElementById("VeiwsAndPaymentsContent").style.display = "block")
+    {  document.getElementById("VeiwsAndPaymentsContent").style.display = "none";
+      document.getElementById("iframes").style.display = "none";
+      document.getElementById("ReadyCaseFormatContent").style.display = "block";
+      if (document.getElementById("AppointmentDetails").style.display = "block") {
+         document.getElementById("AppointmentDetails").style.display = "none";
+       }
     }
 
-}
-
-
-function VisitReport() {
-    document.getElementById("VisitReportButton").style.backgroundColor = "rgb(237, 97, 162)";
-    document.getElementById("VisitReportButton").style.color = "black";
-    document.getElementById("VeiwsAndPaymentsbutton").style.backgroundColor = "white";
-    document.getElementById("VeiwsAndPaymentsbutton").style.color = "grey";
-    document.getElementById("ReadyCaseFormatButton").style.backgroundColor = "white";
-    document.getElementById("ReadyCaseFormatButton").style.color = "grey";
-    //document.getElementById("VisitReportButton").style.borderColor = "grey";
-    if (document.getElementById("AppointmentDetails").style.display = "block") {
-        document.getElementById("AppointmentDetails").style.display = "none";
-    }
-    if (document.getElementById("ReadyCaseFormatContent").style.display = "block") {
-        document.getElementById("ReadyCaseFormatContent").style.display = "none";
-    }
-    if (document.getElementById("VeiwsAndPaymentsContent").style.display = "block") {
-        document.getElementById("VeiwsAndPaymentsContent").style.display = "none";
-    }
-    if (document.getElementById("iframes").style.display = "none") {
-        document.getElementById("iframes").style.display = "block";
-        var f = document.querySelectorAll(".iframe");
-        for (let i = 0; i < f.length; i++) {
-            f[i].style.display = "block";
-        }
-    }
 }

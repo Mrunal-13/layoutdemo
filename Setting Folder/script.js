6a644954eaebc9dd0fe273b4f8f66da116c13263
anju2408 <anjudewangan2000@gmail.com>
2022-05-29 16:57:36 +0530

    Update setting

--- Setting Folder/script.js
+++ Setting Folder/script.js
@@ -37,10 +37,10 @@ $(document).ready(function() {
         clickOtherStaff();
     });
     $("#btn-doctor").click(function() {
-        clickOtherStaff();
-    });
-    $("#btn-other-staff-2").click(function() {
         clickDoctor();
+    });
+    $("#btn-other-staff").click(function() {
+        clickOtherStaff();
     });
     $("#btn-other-doctor").click(function() {
         clickDoctor();
@@ -849,3 +849,57 @@ function clickAddBranch() {
     $(".dvPracticeDetails").css("display", "none");
     $(".dvAddBranch").css("display", "block");
 }
+
+// Map
+var position = [19.1922, 72.9864];
+
+function initialize() {
+    var latlng = new google.maps.LatLng(position[0], position[1]);
+    var options = {
+        zoom: 10,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map(document.getElementById("map"), options);
+
+    marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        title: "Latitude:" + position[0] +
+            " | Longitude:" + position[1]
+    });
+
+    google.maps.event.addListener(map, "click", function(event) {
+        var result = [event.latLng.lat(), event.latLng.lng()];
+        transition(result);
+    });
+};
+
+google.maps.event.addDomListener(window, "load", initialize);
+
+var numDeltas = 100;
+var delay = 5;
+var i = 0;
+var deltaLat;
+var deltaLng;
+
+function transition(result) {
+    i = 0;
+    deltaLat = (result[0] - position[0]) / numDeltas;
+    deltaLng = (result[1] - position[1]) / numDeltas;
+    moveMarker();
+};
+
+function moveMarker() {
+    position[0] += deltaLat;
+    position[1] += deltaLng;
+    var latlng = new google.maps.LatLng(position[0], position[1]);
+    marker.setTitle("Latitude:" + position[0] +
+        " | Longitude:" + position[1]);
+    marker.setPosition(latlng);
+    if (i != numDeltas) {
+        i++;
+        setTimeout(moveMarker, delay);
+    }
+};

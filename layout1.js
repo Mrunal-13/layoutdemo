7699fc4130e3380a8acd7bf2013fb7baf3334b07
Mrunal-13 <mrunalnimbalkar13032001@gmail.com>
2022-06-11 20:35:00 +0530

    add admin pop up

--- layout1.js
+++ layout1.js
@@ -12,7 +12,6 @@ alertsDropdown.addEventListener("click", () => {
 })
 
 
-
 // collapse button function to toggle sidenav barr
 function openNav() {
     if (document.getElementById("mySidenav").style.width === "63px" || document.getElementById("mySidenav").style.width === "") {
@@ -24,23 +23,23 @@ function openNav() {
             document.getElementById("mySidenav").style.width = "63px";
         }
     }
-    if (document.getElementById("leftright").style.width === "95.5%" || document.getElementById("leftright").style.width === "") {
-        document.getElementById("leftright").style.width = "83.5%"
-    } else {
-        document.getElementById("leftright").style.width = "95.5%";
-    }
-    if (document.getElementById("navbar-fourth").style.width === "96%" || document.getElementById("navbar-fourth").style.width === "") {
-        document.getElementById("navbar-fourth").style.width = "83.5%";
-        console.log("83")
-    } else if (document.getElementById("navbar-fourth").style.width === "78%") {
-        document.getElementById("navbar-fourth").style.width = "65%";
-        console.log("65")
-    } else if (document.getElementById("navbar-fourth").style.width === "65%") {
-        document.getElementById("navbar-fourth").style.width = "78%";
-        console.log("78")
-    } else {
-        document.getElementById("navbar-fourth").style.width = "96%";
-    }
+    // if (document.getElementById("leftright").style.width === "95.5%" || document.getElementById("leftright").style.width === "") {
+    //     document.getElementById("leftright").style.width = "83.5%"
+    // } else {
+    //     document.getElementById("leftright").style.width = "95.5%";
+    // }
+    // if (document.getElementById("navbar-fourth").style.width === "96%" || document.getElementById("navbar-fourth").style.width === "") {
+    //     document.getElementById("navbar-fourth").style.width = "83.5%";
+    //     console.log("83")
+    // } else if (document.getElementById("navbar-fourth").style.width === "78%") {
+    //     document.getElementById("navbar-fourth").style.width = "65%";
+    //     console.log("65")
+    // } else if (document.getElementById("navbar-fourth").style.width === "65%") {
+    //     document.getElementById("navbar-fourth").style.width = "78%";
+    //     console.log("78")
+    // } else {
+    //     document.getElementById("navbar-fourth").style.width = "96%";
+    // }
     // if (document.getElementById("commeddiv").style.marginLeft === "1170px" || document.getElementById("commeddiv").style.marginLeft === ""){
     //   document.getElementById("commeddiv").style.marginLeft = "980px"
     // }
@@ -54,15 +53,15 @@ function openNav() {
     //   document.getElementById("commeddiv").style.marginLeft = "900px";
     //   console.log("900")
     // }
-    if (document.getElementById("Appointment").style.marginLeft === "480px" || document.getElementById("Appointment").style.marginLeft === "") {
-        document.getElementById("Appointment").style.marginLeft = "300px";
-    } else if (document.getElementById("Appointment").style.marginLeft === "300px") {
-        document.getElementById("Appointment").style.marginLeft = "480px"
-    } else if (document.getElementById("Appointment").style.marginLeft === "180px") {
-        document.getElementById("Appointment").style.marginLeft = "10px"
-    } else if (document.getElementById("Appointment").style.marginLeft === "10px") {
-        document.getElementById("Appointment").style.marginLeft = "180px"
-    }
+    // if (document.getElementById("Appointment").style.marginLeft === "480px" || document.getElementById("Appointment").style.marginLeft === "") {
+    //     document.getElementById("Appointment").style.marginLeft = "300px";
+    // } else if (document.getElementById("Appointment").style.marginLeft === "300px") {
+    //     document.getElementById("Appointment").style.marginLeft = "480px"
+    // } else if (document.getElementById("Appointment").style.marginLeft === "180px") {
+    //     document.getElementById("Appointment").style.marginLeft = "10px"
+    // } else if (document.getElementById("Appointment").style.marginLeft === "10px") {
+    //     document.getElementById("Appointment").style.marginLeft = "180px"
+    // }
 }
 
 let adminSettingBox = document.getElementById("adminSettingBox");

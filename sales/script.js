03aa44b126d5f8f9459d74285370974475f5da78
aakash <aakashdave21@gmail.com>
2022-08-16 16:26:32 +0530

    added cancle button after file name and some changes in dashboard

--- sales/script.js
+++ sales/script.js
@@ -593,7 +593,23 @@ new Chart("myChart", {
 // get the name of the file 
 let formFileMultiple=document.getElementById("formFileMultiple");
 let fileName=document.querySelector("#fileName");
+let span=document.createElement("span");
+    span.classList.add("iconify");
+    span.setAttribute("data-icon","charm:circle-cross")
+    span.setAttribute("id","cancleName");
+    span.setAttribute("style","color:red")
+    span.setAttribute("data-width","17")
+    span.setAttribute("data-height","17")
+    span.setAttribute("onclick","removeName()")
+
 formFileMultiple.addEventListener("change",()=>{
     let Myfiles=formFileMultiple.files[0];
-    fileName.innerHTML=`${Myfiles.name}`;
+    fileName.innerHTML=`${Myfiles.name}&nbsp;&nbsp;&nbsp;`;
+    fileName.append(span)
+    console.log(fileName);
 })
+
+function removeName(){
+    fileName.innerHTML=""
+}
+
